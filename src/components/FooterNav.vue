95cb17b50f0ad4d7f26e051142ebfe64e9c8a657
MartinGaoC <gcpmws@163.com>
2017-08-22 17:14:27 +0800

    消息

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,21 +1,51 @@
 <template>
-  <div>
-    <div class="footer-nav">
-      <router-link to="#">首页</router-link>
-      <router-link to="#">信息</router-link>
-      <router-link to="#">我的</router-link>
-      <router-link to="#">更多</router-link>
-    </div>
-  </div>
+<mu-paper class="footer-nav">
+  <mu-bottom-nav :value="bottomNav" @change="handleChange" >
+    <mu-bottom-nav-item value="home" title="首页" icon="home" to="/" />
+    <mu-bottom-nav-item value="news" title="消息" icon="email" to="/Vue-app/src/pages/message" />
+    <mu-bottom-nav-item value="my" title="我的" icon="person" :to="person_path" />
+    <mu-bottom-nav-item value="more" title="更多" icon="more" to="#" />
+  </mu-bottom-nav>
+</mu-paper>
 </template>
+
 <script>
-  export default {
-    name: 'FooterNav'
+export default {
+  data () {
+    return {
+      bottomNav: 'recents'
+    }
+  },
+  methods: {
+    handleChange (val) {
+      this.bottomNav = val
+    },
+    facthData () {
+      let accesstoken = localStorage.getItem('accesstoken')
+      if (accesstoken) {
+        this.person_path = '/Vue-app/src/pages/my'
+      } else {
+        this.person_path = '/Vue-app/src/pages/login'
+      }
+      if (this.$route.path === '/') {
+        this.bottomNav = '首页'
+      }
+      if (this.$route.path === '/Vue-home/src/pages/message') {
+        this.bottomNav = '消息'
+      }
+      if (this.$route.page === '/Vue-home/src/pages/login' || this.$route.page === 'Vue-app/src/pages/my') {
+        this.bottomNav = '我的'
+      }
+    }
   }
+}
 </script>
 <style  scoped>
   .footer-nav {
-    width: 100%;
-    background-color: red;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #eee;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '../pages/index.vue'
+import message from '../pages/message.vue'
+import login from '../pages/login.vue'
+import my from '../pages/my.vue'
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/pages/index',
+      path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/Vue-app/src/pages/message',
+      name: 'message',
+      component: message
+    },
+    {
+      path: '/Vue-app/src/pages/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/Vue-app/src/pages/my',
+      name: 'my',
+      component: my
     }
   ]
 })
