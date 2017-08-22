47e254798ef13e69ef55817665c88d1ac6878892
qq2289633869 <2289633869@qq.com>
2017-08-22 09:16:07 +0800

    footerNav

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="footer-nav">
-      <router-link to="#">首页</router-link>
-      <router-link to="#">信息</router-link>
-      <router-link to="#">我的</router-link>
-      <router-link to="#">更多</router-link>
+      <router-link to="#">首页</router-link><router-link to="#">信息</router-link><router-link to="#">我的</router-link><router-link to="#">更多</router-link>
     </div>
   </div>
 </template>
@@ -15,7 +12,18 @@
 </script>
 <style  scoped>
   .footer-nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
     width: 100%;
-    background-color: red;
+    background-color: #eee;
+    border-top: 1px solid #999;
+  }
+  a {
+    text-align: center;
+    color: #3c3c3c;
+    display: inline-block;
+    width: 25%;
+    height: 50px;
   }
 </style>
