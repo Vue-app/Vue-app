c2c8a65817a6a8ea85e41db64f87693a0b040c55
MartinGaoC <gcpmws@163.com>
2017-08-22 22:35:53 +0800

    8-22消息页

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,12 +1,12 @@
 <template>
-<mu-paper class="footer-nav">
-  <mu-bottom-nav :value="bottomNav" @change="handleChange" >
-    <mu-bottom-nav-item value="home" title="首页" icon="home" to="/" />
-    <mu-bottom-nav-item value="news" title="消息" icon="email" to="/Vue-app/src/pages/message" />
-    <mu-bottom-nav-item value="my" title="我的" icon="person" :to="url" />
-    <mu-bottom-nav-item value="more" title="更多" icon="more" to="#" />
-  </mu-bottom-nav>
-</mu-paper>
+  <mu-paper class="footer-nav">
+    <mu-bottom-nav :value="bottomNav" @change="handleChange" >
+      <mu-bottom-nav-item value="home" title="首页" icon="home" to="/" />
+      <mu-bottom-nav-item value="news" title="消息" icon="email" to="/Vue-app/src/pages/message" />
+      <mu-bottom-nav-item value="my" title="我的" icon="person" :to="url" />
+      <mu-bottom-nav-item value="more" title="更多" icon="more" to="/Vue-app/src/pages/more" />
+    </mu-bottom-nav>
+  </mu-paper>
 </template>
 
 <script>
@@ -30,24 +30,24 @@ export default {
     facthData () {
       let accesstoken = localStorage.getItem('accesstoken')
       if (accesstoken) {
-        this.url = '../pages/my'
-      } else {
-        this.url = '../pages/login'
-        console.log(this.url)
         console.log(accesstoken)
+        this.url = '/Vue-app/src/pages/my'
+        console.log(this.url)
+      } else {
+        this.url = '/Vue-app/src/pages/login'
       }
-//      if (this.$route.path === '/') {
-//        this.bottomNav = '首页'
-//      }
-//      if (this.$route.path === '../pages/message') {
-//        this.bottomNav = '消息'
-//      }
-//      if (this.$route.path === '../pages/login' || this.$route.path === '../pages/my') {
-//        this.bottomNav = '我的'
-//      }
-//      if (this.$route.path === '#') {
-//
-//      }
+      if (this.$route.path === '/') {
+        this.bottomNav = '首页'
+      }
+      if (this.$route.path === '/Vue-home/src/pages/message') {
+        this.bottomNav = '消息'
+      }
+      if (this.$route.path === '/Vue-home/src/pages/login' || this.$route.path === '../pages/my') {
+        this.bottomNav = '我的'
+      }
+      if (this.$route.path === '/Vue-home/src/pages/more') {
+        this.bottomNav = '更多'
+      }
     }
   }
 }
