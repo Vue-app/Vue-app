37f28ff1318bfad6edf9165f23cfe3b2c68833c6
ZhangHao <2289633869@qq.com>
2017-09-01 08:59:20 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ import router from './router'
 import Vuex from 'vuex'
 import store from './store'
 import MuseUI from 'muse-ui'
-import infiniteScroll from 'vue-infinite-scroll'
 import 'muse-ui/dist/muse-ui.css'
 import 'muse-ui/dist/theme-carbon.css'
 import * as filters from './util/filter'
 
 Vue.use(MuseUI)
 Vue.use(Vuex)
-Vue.use(infiniteScroll)
 Vue.config.productionTip = false
 // 注册过滤器
 /* eslint-disable no-new */
